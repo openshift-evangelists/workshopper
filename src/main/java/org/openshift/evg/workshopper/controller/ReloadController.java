b498f8f570bbb7177493f428ca1d149cd1a95d25
Siamak Sadeghianfar <ssadeghi@redhat.com>
2017-08-17 17:17:14 +0700

    fixed the content type bug with the reload controller

--- src/main/java/org/openshift/evg/workshopper/controller/ReloadController.java
+++ src/main/java/org/openshift/evg/workshopper/controller/ReloadController.java
@@ -5,11 +5,16 @@ import org.openshift.evg.workshopper.modules.ModulesProvider;
 import org.openshift.evg.workshopper.workshops.WorkshopProvider;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 
 @Path("/reload")
+@Produces({ MediaType.APPLICATION_JSON })
+@Consumes({ MediaType.APPLICATION_JSON })
 public class ReloadController {
 
     @Inject
